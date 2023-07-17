0ef765911a4e89bf49bd0d226da26ef88abbe40d
Waseeq Ahmed <137511755+MDWaseeqAhmed@users.noreply.github.com>
2023-07-17 16:29:05 +0530

    Update LoveScoreCalculator.js

--- LoveScoreCalculator.js
+++ LoveScoreCalculator.js
@@ -1,10 +1,17 @@
 function loveCal(){
  var name1 = prompt("Enter your Name:");
  var name2 = prompt("Enter your Crush's Name:");
- var n = Math.random() * 100;
-  n = Math.floor(n) + 1;
+ var loveScore = Math.random();
+  loveScore = loveScore * 100;
+  loveScore = Math.floor(loveScore) + 1;
  name_1 = name1.charAt(0).toUpperCase() + name1.slice(1);
  name_2 = name2.charAt(0).toUpperCase() + name2.slice(1);
- alert(name_1 + " your crush " + name2 +" Loves you " + n + "%");
+
+ if (loveScore > 75) {
+    alert(name_1 + " your crush " + name2 +" Loves you " + loveScore + "%, your love for each other is like Adam loves Eve!");
+ }else{
+    alert(name_1 + " your crush " + name2 +" Loves you " + loveScore + "%");
+ }
+ 
 }
 loveCal();
